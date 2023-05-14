be197d903798d32421dba450329a56ce7c841b3b
dkkiooko <kithekadankioko@gmail.com>
2023-05-14 14:51:35 +0300

    added missing comment under function get_pages

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,11 +34,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ gets pages from the input pagination requirements """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
         try:
             data = self.dataset()
             start, stop = index_range(page=page, page_size=page_size)
             return data[start: stop]
-        except IndexError:
+        except Exception:
             return []
